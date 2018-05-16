eee79a55fdde7f80854a0b32cfb51802c9f737f2
Dmitry Petrov <dimaninc@gmail.com>
2018-05-16 10:49:56 +0300

    init.sh: Macros

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # starter
 diCMS project starter
+
+## Usage:
+`git clone https://github.com/dimaninc/starter.git Folder && sh Folder/scripts/init.sh`
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,68 @@
 #!/usr/bin/env bash
 
+outer_path=$(pwd)
+path="$outer_path/$0"
+defaultUser='dimaninc'
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	isMacOs=true
+	sedParam=" ''"
+else
+	isMacOs=false
+	sedParam=''
+fi
+
+if [[ $path =~ ((.*)/([^/]+)/scripts/init.sh)$ ]]; then
+	folder=${BASH_REMATCH[3]}
+	root="${BASH_REMATCH[2]}/$folder"
+	echo "Root path: $root"
+    echo "Website folder: $folder"
+else
+	echo "Unable to extract website folder from the path '$folder'"
+	exit
+fi
+
+while [ -z ${domain} ]; do
+     read -p 'Main domain: ' domain
+done
+while [ -z ${namespace} ]; do
+     read -p 'Namespace: ' namespace
+done
+read -p "User [${defaultUser}]: " user
+user=${user:-$defaultUser}
+
+creator='diCMS Starter'
+date=$(date +"%d.%m.%Y")
+time=$(date +"%H:%M:%S")
+
 # replace macros
-'NewProject'
-'[%CREATOR%]'
-'[%USER%]'
-'[%DATE%]'
-'[%TIME%]'
-'[%DOMAIN%]'
-'[%FOLDER%]'
+folderToken='\[%FOLDER%\]'
+domainToken='\[%DOMAIN%\]'
+namespaceToken='NewProject'
+userToken='\[%USER%\]'
+creatorToken='\[%CREATOR%\]'
+dateToken='\[%DATE%\]'
+timeToken='\[%TIME%\]'
+
+cd "$root"
+echo 'Setting Variables...'
+find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" -e "s/$folderToken/$folder/g" -e "s/$domainToken/$domain/g" -e "s/$namespaceToken/$namespace/g" -e "s/$userToken/$user/g" -e "s/$creatorToken/$creator/g" -e "s/$dateToken/$date/g" -e "s/$timeToken/$time/g"
+
+#echo 'Setting Folder...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$folderToken/$folder/g"
+#echo 'Setting Domain...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$domainToken/$domain/g"
+#echo 'Setting Namespace...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$namespaceToken/$namespace/g"
+#echo 'Setting User...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$userToken/$user/g"
+#echo 'Setting Creator...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$creatorToken/$creator/g"
+#echo 'Setting Date...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$dateToken/$date/g"
+#echo 'Setting Time...'
+#find . -type f ! -name 'init.sh' | xargs sed -i"$sedParam" "s/$timeToken/$time/g"
+cd - >/dev/null 2>&1
 
 # generate Environment class
 
@@ -19,7 +74,7 @@
 
 # gulp build
 
-# kill git folder
+# kill .git folder
 
 # self-destruction
 #rm $0
