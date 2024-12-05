b7a6901e037f55109971a476993fd0bdb94288b6
Dmitry Petrov <dimaninc@gmail.com>
2024-12-05 10:22:21 +0300

    updated sample.sql and code style configs

--- db/dump/tables/sample.sql
+++ db/dump/tables/sample.sql
@@ -1,14 +1,19 @@
 CREATE TABLE IF NOT EXISTS sample(
-  id bigint unsigned not null auto_increment,
-  user_id bigint DEFAULT '0',
+  id int unsigned not null auto_increment,
+  user_id int DEFAULT '0',
   name varchar(100) default '',
   email varchar(60) default '',
   phone varchar(20) default '',
   company varchar(100) default '',
   web varchar(100) default '',
   content text,
+  properties json,
   ip bigint,
-  date timestamp default CURRENT_TIMESTAMP,
+  order_num int,
+  visible tinyint default '0',
+  top tinyint default '0',
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default NULL,
   primary key(id),
-  index idx(name, email, company)
+  index idx(name, email, company, visible, top, order_num)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
